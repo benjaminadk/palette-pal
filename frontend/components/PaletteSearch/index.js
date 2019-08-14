e818e651f9a3050c6623db52d383a09d66067d8a
benjaminadk <benjaminadk@gmail.com>
2019-08-14 10:48:57 -0700

    :recycle: Refactor palettes page

--- frontend/components/PaletteSearch/index.js
+++ frontend/components/PaletteSearch/index.js
@@ -1,26 +1,38 @@
-import { AppContext } from '../Layout'
-import TextInput from '../TextInput'
-import RadioButtons from './RadioButtons'
-import { PaletteSearchWrapper, SearchInterface } from './styles'
+import { useRef, useState } from 'react'
+import PaletteSearchBar from '../PaletteSearchBar'
+import PaletteList from '../PaletteList'
+import { PaletteSearchWrapper } from './styles'
 
-const PalettesSearch = ({ searchRef }) => {
+const Palettes = ({ pathname, palettes, onAvatarClick, fetchMorePalettes }) => {
+  const [showAutoReturn, setShowAutoReturn] = useState(false)
+
+  const search = useRef(null)
+
+  function onScroll(e, fetchMorePalettes) {
+    const { scrollTop, scrollHeight, clientHeight } = e.currentTarget
+    if (scrollTop + clientHeight >= scrollHeight) {
+      fetchMorePalettes()
+    }
+
+    setShowAutoReturn(scrollTop > 0)
+  }
+
+  function onAutoReturnClick() {
+    search.current.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' })
+  }
+
   return (
-    <AppContext.Consumer>
-      {({ searchTerm, onSearchTermChange }) => (
-        <PaletteSearchWrapper ref={searchRef}>
-          <SearchInterface>
-            <TextInput
-              type='text'
-              placeholder='Search'
-              value={searchTerm}
-              onChange={onSearchTermChange}
-            />
-            <RadioButtons />
-          </SearchInterface>
-        </PaletteSearchWrapper>
-      )}
-    </AppContext.Consumer>
+    <PaletteSearchWrapper onScroll={e => onScroll(e, fetchMorePalettes)}>
+      <PaletteSearchBar searchRef={search} />
+      <PaletteList
+        pathname={pathname}
+        palettes={palettes}
+        showAutoReturn={showAutoReturn}
+        onAvatarClick={onAvatarClick}
+        onAutoReturnClick={onAutoReturnClick}
+      />
+    </PaletteSearchWrapper>
   )
 }
 
-export default PalettesSearch
+export default Palettes
--- frontend/pages/palettes.js
+++ frontend/pages/palettes.js
@@ -1,48 +1,17 @@
-import { useRef, useState } from 'react'
 import { AppContext } from '../components/Layout'
 import PaletteSearch from '../components/PaletteSearch'
-import PaletteList from '../components/PaletteList'
-import styled from 'styled-components'
 
-export const PalettesWrapper = styled.div`
-  height: calc(100vh - ${p => p.theme.headerHeight}px);
-  overflow: auto;
-`
-
-const Palettes = ({ pathname }) => {
-  const [showAutoReturn, setShowAutoReturn] = useState(false)
-
-  const search = useRef(null)
-
-  function onScroll(e, fetchMorePalettes) {
-    const { scrollTop, scrollHeight, clientHeight } = e.currentTarget
-    if (scrollTop + clientHeight >= scrollHeight) {
-      fetchMorePalettes()
-    }
-
-    setShowAutoReturn(scrollTop > 0)
-  }
-
-  function onAutoReturnClick() {
-    search.current.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest' })
-  }
-
+export default ({ pathname }) => {
   return (
     <AppContext.Consumer>
       {({ loading, palettes, onAvatarClick, fetchMorePalettes }) => (
-        <PalettesWrapper onScroll={e => onScroll(e, fetchMorePalettes)}>
-          <PaletteSearch searchRef={search} />
-          <PaletteList
-            pathname={pathname}
-            palettes={palettes}
-            showAutoReturn={showAutoReturn}
-            onAvatarClick={onAvatarClick}
-            onAutoReturnClick={onAutoReturnClick}
-          />
-        </PalettesWrapper>
+        <PaletteSearch
+          pathname={pathname}
+          palettes={palettes}
+          onAvatarClick={onAvatarClick}
+          fetchMorePalettes={fetchMorePalettes}
+        />
       )}
     </AppContext.Consumer>
   )
 }
-
-export default Palettes
